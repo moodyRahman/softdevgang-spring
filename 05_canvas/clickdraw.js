fc4cc65d20854e9de148a9f16babf890589b337c
Moody Rahman <mrahman01@stuy.edu>
2020-02-06 20:21:29 -0500

    code cleanup

--- 05_canvas/clickdraw.js
+++ 05_canvas/clickdraw.js
@@ -1,6 +1,6 @@
-
-
 var curr_state = "circle"
+
+
 const canvas = document.getElementById("mood");
 const button = document.getElementById("toggle");
 const status = document.getElementById("status");
@@ -25,56 +25,29 @@ var toggle = function(e){
 
 };
 
-
-button.addEventListener("click", toggle);
-clear.addEventListener("click", function(){ctx.clearRect(0, 0, canvas.width, canvas.height);});
-
-
-canvas.addEventListener("mousemove", function(e, state){draw(e, curr_state)});
-
-
-
-
 var clearcanvas = function(canvas, contex){
 	context.clearRect(0, 0, canvas.width, canvas.height);
 };
 
-var getMousePos = function(e) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-    };
-};
-
-
 var draw = function (e, state){
 	var rect = canvas.getBoundingClientRect();
-
 	const x = e.clientX - rect.left;
 	const y = e.clientY - rect.top;
-
-	console.log(state, x, y, e);
 
 	if (state === "circle"){
 		ctx.beginPath();
 		ctx.arc(x, y, 5, 0, 2 * Math.PI);
 		ctx.stroke();
-
 	}
 	else {
 		ctx.beginPath();
 		ctx.rect(x, y, 8, 8);
 		ctx.stroke();
-	
 	}
-
-
-
-
-
 };
 
 
 
-
+button.addEventListener("click", toggle);
+clear.addEventListener("click", function(){ctx.clearRect(0, 0, canvas.width, canvas.height);});
+canvas.addEventListener("mousemove", function(e, state){draw(e, curr_state)});

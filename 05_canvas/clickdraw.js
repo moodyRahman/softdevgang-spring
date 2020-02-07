1ad5f34d95aed147d22c11e9b1477e1a0190e11d
Moody Rahman <mrahman01@stuy.edu>
2020-02-06 20:19:01 -0500

    clear painting

---
+++ 05_canvas/clickdraw.js
@@ -0,0 +1,80 @@
+
+
+var curr_state = "circle"
+const canvas = document.getElementById("mood");
+const button = document.getElementById("toggle");
+const status = document.getElementById("status");
+const clear = document.getElementById("clear");
+const ctx = canvas.getContext("2d");
+
+
+var toggle = function(e){
+	if (curr_state === "circle"){
+		curr_state = "rect";
+		status.innerHTML = "rectangle";
+	}
+	
+	else{
+		curr_state = "circle";
+		status.innerHTML = "circle";
+
+	}
+
+	console.log("current state is");
+	console.log(curr_state);
+
+};
+
+
+button.addEventListener("click", toggle);
+clear.addEventListener("click", function(){ctx.clearRect(0, 0, canvas.width, canvas.height);});
+
+
+canvas.addEventListener("mousemove", function(e, state){draw(e, curr_state)});
+
+
+
+
+var clearcanvas = function(canvas, contex){
+	context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+var getMousePos = function(e) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+};
+
+
+var draw = function (e, state){
+	var rect = canvas.getBoundingClientRect();
+
+	const x = e.clientX - rect.left;
+	const y = e.clientY - rect.top;
+
+	console.log(state, x, y, e);
+
+	if (state === "circle"){
+		ctx.beginPath();
+		ctx.arc(x, y, 5, 0, 2 * Math.PI);
+		ctx.stroke();
+
+	}
+	else {
+		ctx.beginPath();
+		ctx.rect(x, y, 8, 8);
+		ctx.stroke();
+	
+	}
+
+
+
+
+
+};
+
+
+
+

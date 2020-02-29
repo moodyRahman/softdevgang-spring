c69a252ef4126f8bd78b49fcbcecd439e9e85752
Moody Rahman <mrahman01@stuy.edu>
2020-02-28 20:11:14 -0500

    query by title

---
+++ 10_mongo/mongo.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+import json
+
+client = MongoClient("mongodb://localhost:27017/")
+
+db = client["test"]
+
+collection = db["movies"]
+
+m_count = collection.count_documents({})
+
+if m_count == 0:
+    toinsert = json.loads(open("movies.json", "r").read())
+    for movie in toinsert:
+        # print(type(toinsert))
+        collection.insert_one(movie)
+
+
+# {"title":"The Thunderbolt","year":1912,"cast":["William Garwood","James Cruze","David Thompson"],"genres":["Drama"]},
+
+def query_title(title):
+    query = {"title":{"$regex":title, "$options": "i"}}
+
+    result = collection.find(query)
+
+    out = []
+    for movie in result:
+        # print(movie)
+        out.append(movie)
+    return out
+
+
+
+
+d = query_title("ood")

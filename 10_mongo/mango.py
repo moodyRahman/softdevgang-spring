b06de269df162511e5e4b8e423dfa5aef48d53cd
Moody Rahman <mrahman01@stuy.edu>
2020-03-02 13:37:36 -0500

    tabbed spaces

--- 10_mongo/mango.py
+++ 10_mongo/mango.py
@@ -3,30 +3,30 @@
 # K10 -- Import/Export Bank
 # 2020-02-29
 '''
-    name of dataset and description of its contents
-    hyperlink to where raw data is hosted
-    brief summary of your import mechanism
-    
-    movies scraped from wikipedia
-    https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json
-    
-    
-    import mechanism
-    
-    this is done with code that is garunteed to run as either an import or when directly executed
-    it establishes a connection to mongod and switches to the "movies" collection in the "test" db
-    it then counts how many documents there are in "movies" and if it's zero, parse the movies.json and insert each element
-    if there are documents inside "movies" then it will do nothing
-    
+	name of dataset and description of its contents
+	hyperlink to where raw data is hosted
+	brief summary of your import mechanism
+	
+	movies scraped from wikipedia
+	https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json
+	
+	
+	import mechanism
+	
+	this is done with code that is garunteed to run as either an import or when directly executed
+	it establishes a connection to mongod and switches to the "movies" collection in the "test" db
+	it then counts how many documents there are in "movies" and if it's zero, parse the movies.json and insert each element
+	if there are documents inside "movies" then it will do nothing
+	
 
-    the query object was meant to facillitate query's with multiple parameters 
-    multiple query calls can be formatted as 
-    q = Query() 
-    q.actor("mood").afteryear(2002)
-    --or--
-    q = Query()
-    q.actor("mood")
-    q.afteryear(2002)
+	the query object was meant to facillitate query's with multiple parameters 
+	multiple query calls can be formatted as 
+	q = Query() 
+	q.actor("mood").afteryear(2002)
+	--or--
+	q = Query()
+	q.actor("mood")
+	q.afteryear(2002)
 
 '''
 
@@ -44,170 +44,169 @@ collection = db["movies"]
 m_count = collection.count_documents({})
 
 if m_count == 0:
-    toinsert = json.loads(open("movies.json", "r").read())
-    for movie in toinsert:
-        # print(type(toinsert))
-        collection.insert_one(movie)
+	toinsert = json.loads(open("movies.json", "r").read())
+	for movie in toinsert: 
+		# print(type(toinsert))
+		collection.insert_one(movie)
 
 
 # {"title":"The Thunderbolt","year":1912,"cast":["William Garwood","James Cruze","David Thompson"],"genres":["Drama"]},
 
 
 class Query(object):
-    """Create a query to be executed
+	"""Create a query to be executed
 
-    Attributes
-    ----------
-    query : dict
-        inner query dictionary
-    query_out : list
-        stores the out of an execute()
+	Attributes
+	----------
+	query : dict
+		inner query dictionary
+	query_out : list
+		stores the out of an execute()
 
-    """
+	"""
 
-    def __init__(self):
-        """ Initialize a query """
-        super(Query, self).__init__()
-        self.query = {"$and":[]}
-        self.query_out = []
+	def __init__(self):
+		""" Initialize a query """
+		super(Query, self).__init__()
+		self.query = {"$and":[]}
+		self.query_out = []
 
-    
-    def genre(self, gen):
+	
+	def genre(self, gen):
 
-        """Specify a genre to search the database by
+		"""Specify a genre to search the database by
 
-        Parameters
-        ----------
-        gen : String
-            Name of genre to query by
+		Parameters
+		----------
+		gen : String
+			Name of genre to query by
 
-        Returns
-        -------
-        Query
-            this instance of Query
+		Returns
+		-------
+		Query
+			this instance of Query
 
-        """
+		"""
 
-        self.query["$and"].append({"genres":{"$regex":gen, "$options": "i"}})
-        return self
-
-
-    def title(self, name):
-
-        """Specify a title to search the database by
-
-        Parameters
-        ----------
-        name : type
-            Name of movie to query for.
-
-        Returns
-        -------
-        Query
-            this instance of Query.
-
-        """
-
-        self.query["$and"].append({"title":{"$regex":name, "$options": "i"}})
-        return self
+		self.query["$and"].append({"genres":{"$regex":gen, "$options": "i"}})
+		return self
 
 
-    def actor(self, name):
+	def title(self, name):
 
-        """Specify an actor to search the database by
+		"""Specify a title to search the database by
 
-        Parameters
-        ----------
-        name : String
-            name of actor to query for.
+		Parameters
+		----------
+		name : type
+			Name of movie to query for.
 
-        Returns
-        -------
-        Query
-            this instance of Query
+		Returns
+		-------
+		Query
+			this instance of Query.
 
-        """
+		"""
 
-        self.query["$and"].append({"cast":{"$regex":name, "$options": "i"}})
-        return self
-
-
-    def beforeyear(self, before):
-        
-        """Specify what year after to search the database by
-
-        Parameters
-        ----------
-        before : int
-            Which year the query should search before
-
-        Returns
-        -------
-        Query
-            this instance of Query
-
-        """
-        
-        self.query["$and"].append({"year":{"$lte":before}})
-        return self
+		self.query["$and"].append({"title":{"$regex":name, "$options": "i"}})
+		return self
 
 
-    def afteryear(self, after):
-        
-        """Specify what year after to search the database by.
+	def actor(self, name):
 
-        Parameters
-        ----------
-        after : int
-            What year should the query search after
+		"""Specify an actor to search the database by
 
-        Returns
-        -------
-        Query
-            this instance of Query
+		Parameters
+		----------
+		name : String
+			name of actor to query for.
 
-        """
-        
-        self.query["$and"].append({"year":{"$gte":after}})
-        return self
+		Returns
+		-------
+		Query
+			this instance of Query
 
+		"""
 
-    def execute(self):
-        
-        """Execute the query and set query_out to the results
-
-        Returns
-        -------
-        list
-            list containing a deep copy of query_out
-
-        """
-        
-        out = []
-        result = collection.find(self.query)
-        for x in result:
-            out.append(x)
-        self.query_out = out[:]      # store a copy of the array
-        return out
+		self.query["$and"].append({"cast":{"$regex":name, "$options": "i"}})
+		return self
 
 
-    def pretty_print(self, limit=2147483647, order=1, count=False):
-        
-        """Print the results of a query formatted.
+	def beforeyear(self, before):
+		
+		"""Specify what year after to search the database by
 
-        Parameters
-        ----------
-        limit : int
-            how many results to show
-        order : int, -1 or 1
-            Present results in forward to reverse order
-        count : boolean
-            Head the results with the number of total results
-        """
-        
-        if count:
-            print(str(len(self.query_out)) + " results found")    
-        for n, x in enumerate(self.query_out[::order]):
-            if n < limit:
-                print(x)
+		Parameters
+		----------
+		before : int
+			Which year the query should search before
 
+		Returns
+		-------
+		Query
+			this instance of Query
+
+		"""
+		
+		self.query["$and"].append({"year":{"$lte":before}})
+		return self
+
+
+	def afteryear(self, after):
+		
+		"""Specify what year after to search the database by.
+
+		Parameters
+		----------
+		after : int
+			What year should the query search after
+
+		Returns
+		-------
+		Query
+			this instance of Query
+
+		"""
+		
+		self.query["$and"].append({"year":{"$gte":after}})
+		return self
+
+
+	def execute(self):
+		
+		"""Execute the query and set query_out to the results
+
+		Returns
+		-------
+		list
+			list containing a deep copy of query_out
+
+		"""
+		
+		out = []
+		result = collection.find(self.query)
+		for x in result:
+			out.append(x)
+		self.query_out = out[:]      # store a copy of the array
+		return out
+
+
+	def pretty_print(self, limit=2147483647, order=1, count=False):
+		
+		"""Print the results of a query formatted.
+
+		Parameters
+		----------
+		limit : int
+			how many results to show
+		order : int, -1 or 1
+			Present results in forward to reverse order
+		count : boolean
+			Head the results with the number of total results
+		"""
+		
+		if count:
+			print(str(len(self.query_out)) + " results found")    
+		for n, x in enumerate(self.query_out[::order]):
+			if n < limit:
+				print(x)

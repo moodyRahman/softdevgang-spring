cb6283fbc58fd4e63986d6759ff5dda81f50f12d
Moody Rahman <mrahman01@stuy.edu>
2020-03-30 16:11:44 -0400

    god hath saiud hello

--- 13_svg/svg.js
+++ 13_svg/svg.js
@@ -7,56 +7,57 @@ const w3svg = "http://www.w3.org/2000/svg";
 const image = document.getElementById("vimage");
 const clearbutton = document.getElementById("clear");
 
-let x_last = y_last = -1;
 let circles = [];
-const radius = 8;
+const radius = 10;
 
 function dot(x, y) {
 	let circle = document.createElementNS(w3svg, "circle");
 	circle.setAttribute("cx", x);
 	circle.setAttribute("cy", y);
-	circle.setAttribute("r", 8);
-	circle.setAttribute("ischange", 0);
-	circle.addEventListener("click", function(){ changecolor(circle) });
+	circle.setAttribute("r", radius);
+	circle.setAttribute("fill", "red");
 	return circle;
 };
 
-function line(x_1, y_1, x_2, y_2) {
-	let line = document.createElementNS(w3svg, "line");
-	line.setAttribute("x1", x_1);
-	line.setAttribute("y1", y_1);
-	line.setAttribute("x2", x_2);
-	line.setAttribute("y2", y_2);
-	line.setAttribute("stroke", "black");
-	return line;
-};
-
 function draw(e) {
+	console.log(e);
 	let c = dot(e.offsetX, e.offsetY)
-	image.appendChild(c);
+	e.preventDefault()
+
+	if (e.target == image){
+		image.appendChild(c);
+	}
+	
+	c.addEventListener("click", changecolor);
+	
 
 };
 
 
 function clear() {
 	image.innerHTML = "";
-	x_last = y_last = -1;mouselocater
+	x_last = y_last = -1;
 
 };
 
-function changecolor(circle){
-	if (circle.getAttribute("ischange") == 0){
-		circle.setAttribute("ischange", 1);
-		return;
+function changecolor(e){
+	let c = e.target;
+	if (c.getAttribute("fill") == "red") {
+		c.setAttribute("fill", "blue");
 	}
+	else {
+		image.removeChild(c);
+		let newc = dot(
+			Math.floor(Math.random() * 501), 
+			Math.floor(Math.random() * 501) 
+		);
 
-	if (circle.getAttribute("ischange") == 1){
-		circle.setAttribute("fill", "red");
+		image.appendChild(newc);
+		e.preventDefault();
+		newc.addEventListener("click", changecolor);
 	}
-
-	console.log(circle);
-
 };
+
 
 image.addEventListener('click', draw);
 clearbutton.addEventListener('click', clear);
